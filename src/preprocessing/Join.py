5ecc2083dc89857e3be8f817185e0afad9d5d954
Arthurion <jansen.arthur@gmail.com>
2020-03-27 18:16:34 +0100

    Modifications on Join class.

--- src/preprocessing/Join.py
+++ src/preprocessing/Join.py
@@ -13,17 +13,17 @@ class Join:
     @staticmethod
     def inner_join(df_1, df_2):
         inner_join_df = df_1.copy()
-        return inner_join_df.join(df_2, how='inner')
+        return inner_join_df.join(df_2, on=df_1.index.name, how='inner')
 
     @staticmethod
     def left_join(df_1, df_2):
         left_join_df = df_1.copy()
-        return left_join_df.join(df_2, how='left')
+        return left_join_df.join(df_2, on=df_1.index.name, how='left')
 
     @staticmethod
     def right_join(df_1, df_2):
         right_join_df = df_1.copy()
-        return right_join_df.join(df_2, how='right')
+        return right_join_df.join(df_2, on=df_1.index.name, how='right')
 
     def join(self, df_1, df_2):
         return self.dict_join[self.type_join](df_1, df_2)
@@ -32,13 +32,11 @@ class Join:
 if __name__ == "__main__":
     df_1 = pd.DataFrame({'key': ['K0', 'K1', 'K2', 'K3', 'K4', 'K5'],
                          'A': ['A0', 'A1', 'A2', 'A3', 'A4', 'A5']})
-    df_1.set_index('key')
-    print(df_1.keys())
+    df_1.set_index('key', inplace=True)
 
     df_2 = pd.DataFrame({'key': ['K0', 'K1', 'K2'],
                          'B': ['B0', 'B1', 'B2']})
-    df_2.set_index('key')
-    print(df_2.keys())
+    df_2.set_index('key', inplace=True)
 
     join_instance = Join(type_join='inner')
     new_df = join_instance.join(df_1, df_2)
